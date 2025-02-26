7eb5449b25a460f7d7bcf63c6d12521852128625
konradgj <konrad.gjerstad@gmail.com>
2025-02-26 23:07:28 +0100

    CH2: L6 textnode to htmlnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -22,6 +22,31 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("a", TextType.italic)
         self.assertEqual(node.url, None)
 
+    def test_text(self):
+        tnode = TextNode("test", TextType.text)
+        hnode = tnode.toHTMLNode()
+        self.assertEqual(hnode.tag, None)
+        self.assertEqual(hnode.value, "test")
+
+    def test_textTypes(self):
+        tnode1 = TextNode("test", TextType.bold)
+        tnode2 = TextNode("test", TextType.italic)
+        tnode3 = TextNode("test", TextType.code)
+        hnode1 = tnode1.toHTMLNode()
+        hnode2 = tnode2.toHTMLNode()
+        hnode3 = tnode3.toHTMLNode()
+        self.assertEqual(hnode1.tag, "b")
+        self.assertEqual(hnode2.tag, "i")
+        self.assertEqual(hnode3.tag, "code")
+
+    def test_links(self):
+        tnode = TextNode("test", TextType.link, "www.test.no")
+        tnode1 = TextNode("test", TextType.image, "www.test.no")
+        hnode = tnode.toHTMLNode()
+        hnode1 = tnode1.toHTMLNode()
+        self.assertEqual(hnode.propsToHTML(), ' href="www.test.no"')
+        self.assertEqual(hnode1.propsToHTML(), ' src="www.test.no" alt="test"')
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,10 @@
 from enum import Enum
 
+from htmlnode import HTMLNode
+
 
 class TextType(Enum):
-    normal = "normal"
+    text = "text"
     bold = "bold"
     italic = "italic"
     code = "code"
@@ -23,3 +25,18 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.textType.value}, {self.url})"
+
+    def toHTMLNode(self):
+        match self.textType:
+            case TextType.text:
+                return HTMLNode(None, self.text)
+            case TextType.bold:
+                return HTMLNode("b", self.text)
+            case TextType.italic:
+                return HTMLNode("i", self.text)
+            case TextType.code:
+                return HTMLNode("code", self.text)
+            case TextType.link:
+                return HTMLNode("a", self.text, None, {"href": self.url})
+            case TextType.image:
+                return HTMLNode("img", "", None, {"src": self.url, "alt": self.text})
